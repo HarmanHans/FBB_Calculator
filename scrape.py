1cd6630570835664c0ce3da552681a5f053afb3c
HarmanHans <harmanhans56@gmail.com>
2023-01-05 17:19:06 -0800

    got today's date

---
+++ scrape.py
@@ -0,0 +1,19 @@
+import os
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+from datetime import date 
+import json
+import pymongo
+from pymongo import MongoClient
+
+# needs to be until the last day of the NBA regular season
+
+date = str(date.today())
+dateString = date.split("-")
+year = dateString[0]
+month = dateString[1]
+day = dateString[2]
+
+url = f"https://www.basketball-reference.com/boxscores/?month={month}&day={day}&year={year}"
+print(url)
