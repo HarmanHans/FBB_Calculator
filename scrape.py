aba7c1dfad423ca7beeae81f31b0db1518a0ef94
HarmanHans <harmanhans56@gmail.com>
2023-01-06 23:36:34 -0800

    daily scraper stuff

--- scrape.py
+++ scrape.py
@@ -3,13 +3,14 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 from datetime import date 
+from datetime import timedelta
 import json
 import pymongo
 from pymongo import MongoClient
 
 # needs to be until the last day of the NBA regular season
 
-date = str(date.today())
+date = str(date.today() - timedelta(days = 1))
 dateString = date.split("-")
 year = dateString[0]
 month = dateString[1]
