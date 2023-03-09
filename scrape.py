f6e9443aa72cec017982184c5e91173c434c353e
HarmanHans <harmanhans56@gmail.com>
2023-03-08 23:30:32 -0800

    test

--- scrape.py
+++ scrape.py
@@ -58,6 +58,7 @@ if ((intDate - lastDay).days <= 0):
         chrome_options.add_argument("enable-automation")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-browser-side-navigation")
+        chrome_options.add_argument("--dns-prefetch-disable")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--headless")
