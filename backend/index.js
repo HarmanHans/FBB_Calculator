81047eb836d9a578cfdbd4ef77eb5a843c085e85
HarmanHans <harmanhans56@gmail.com>
2023-03-27 16:05:20 -0700

    setting up mongo connection to website

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler.js');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 app.use('/', routesHandler);
 
-const PORT = 4000; // backend routing port
+const PORT = process.env.PORT || 4000; // backend routing port
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology:true})
+.then( () => {
+    console.log('MongoDB Connected!');
+})
+.catch( (err) => {
+    console.log(err);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
--- frontend/src/components/Calculator.js
+++ frontend/src/components/Calculator.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 function Calculator() {
+    useEffect( () => {
+        fetchItems();
+    }, []);
+
+    const [items, setItems] = useState([]);
+    const fetchItems = async() => {
+        const data = await fetch('/calculator');
+        const items = await data.json();
+        setItems(items);
+    }
+
     return(
         <section>
-            <p>Calculator</p>
+            {
+                items.map(item => (
+                    <div>
+                        <p>{item.name}</p>
+                        <p>{item.pts}</p>
+                        <p>{item.ast}</p>
+                    </div>
+                ))
+            }
         </section>
     );
 }
